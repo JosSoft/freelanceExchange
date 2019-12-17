735cf231ba03382791ede74f4b25b9d4ec5b1e3a
Eugene Podbereznykh <podbereznykh@mail.ru>
2019-12-17 16:28:33 +0500

    первый день занятий

--- script/script.js
+++ script/script.js
@@ -1,21 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict'
-
-    /*
-        <a href="#" id="customer" class="btn btn-primary my-3 px-4 py-2">Заказчик</a>
-	    <a href="#" id="freelancer" class="btn btn-secondary my-3 px-4 py-2">Фрилансер</a>
-    */
+    //
     const customer = document.getElementById('customer')
     const freelancer = document.getElementById('freelancer')
+    // Blocks
+    const blockChoice = document.getElementById('block-choice')
+    const blockCustomer = document.getElementById('block-customer')
+    const blockFreelancer = document.getElementById('block-freelancer')
+    const btnExit = document.getElementById('btn-exit')
+    // Froms
+    const formCustomer = document.getElementById('form-customer')
+    // Заказы
+    const orders = []
 
-    customer.addEventListener('contextmenu', () => {
-        console.log('contextmenu');
+
+    customer.addEventListener('click', () => {
+        blockChoice.style.display = 'none'
+        blockCustomer.style.display = 'block'
+        btnExit.style.display = 'block'
+    })
+    freelancer.addEventListener('click', () => {
+        blockChoice.style.display = 'none'
+        blockFreelancer.style.display = 'block'
+        btnExit.style.display = 'block'
+    })
+    btnExit.addEventListener('click', () => {
+        btnExit.style.display = 'none'
+        blockCustomer.style.display = 'none'
+        blockFreelancer.style.display = 'none'
+        blockChoice.style.display = 'block'
+    })
+
+    formCustomer.addEventListener('submit', event => {
+        event.preventDefault()
+        // получаем все елементы
+        const obj = {}
+        //debugger
+
+        for (const elem of formCustomer.elements) {
+            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||            
+            (elem.type === 'radio' && elem.checked) || 
+            elem.tagName === 'TEXTAREA') {
+                obj[elem.name] = elem.value
+                
+                // очищаем значения
+                if (elem.type !== 'radio') {
+                    elem.value = ''
+                }
+            }
+        }
+        orders.push(obj)
+
+        
+        // ДЗ через фильтр переписать
+        //elements.filter(()=>{})
+
+        console.log(orders)
     })
 
 
-
-    //console.log(customer)
-
-
-    //console.log(freelancer)
 })
